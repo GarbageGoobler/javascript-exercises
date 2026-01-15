ad9f2cf680e8d84a0d54968e0d3f9d7dbfdc59cb
GarbageGoobler <velocityraptor314@proton.me>
2026-01-15 16:38:12 +0100

    Solve foundations/13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleaned = str
+    .toLowerCase()
+    .split('')
+    .filter(char => alphanumerical.includes(char))
+    .join('') 
+  return cleaned.split('').reverse().join('') === cleaned;
 };
 
 // Do not edit below this line
