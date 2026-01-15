2317af1f401bc6d51cd379ba2fdcec51aefed847
GarbageGoobler <velocityraptor314@proton.me>
2026-01-15 16:18:18 +0100

    Solve foundations/12_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;	
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, value) => sum + value, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, value) => product * value, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
