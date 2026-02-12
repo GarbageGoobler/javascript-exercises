9c57b4e7940e74580c9f7cf6294fb28fefca5562
GarbageGoobler <velocityraptor314@proton.me>
2026-02-12 01:39:21 +0100

    Solve contains recursion problem

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function (object, target) {
+  if (Object.is(object, target)) return true;
+  if (object === null || typeof object !== "object") {
+    return false;
+  }
+  for (const value of Object.values(object)) {
+    if (contains(value, target)) return true;
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
