43596a0d627cbb9f73b70403800067ff58a1a92e
GarbageGoobler <velocityraptor314@proton.me>
2026-02-12 12:27:11 +0100

    Solve pascal triangle problem in CS

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,15 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+  if (n > 1) {
+    const rowBefore = pascal(n - 1);
+    rowBefore.unshift(0); // add zero as first element
+    // a[n+1] is undefined so gives zero
+    const thisRow = rowBefore.map((num, i, a) => num + (a[i + 1] ?? 0));
+    return thisRow;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
