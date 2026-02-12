f002b0c5d5411302ac957a97ad5af0f638e1f4dc
GarbageGoobler <velocityraptor314@proton.me>
2026-02-12 12:03:15 +0100

    Solve CS permutations problem
    That was cool!

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,26 @@
-const permutations = function() {
-  
+const permutations = function (data) {
+  if (data.length === 0) return [[]];
+  if (data.length === 1) return [data];
+  if (data.length === 2) {
+    const [a, b] = data;
+    return [
+      [a, b],
+      [b, a],
+    ];
+  }
+  if (data.length > 2) {
+    const perms = [];
+    for (let i = 0; i < data.length; i++) {
+      const first = data[i];
+      const rest = [...data.slice(0, i), ...data.slice(i + 1)];
+      const restPerms = permutations(rest);
+      for (const perm of restPerms) {
+        perms.push([first, ...perm]);
+      }
+    }
+    return perms;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
