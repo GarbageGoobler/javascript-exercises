2cee5ba2774b7d5bb820b7a2983b353e8c842678
GarbageGoobler <velocityraptor314@proton.me>
2026-02-12 11:36:19 +0100

    Solve totalIntegers CS problem

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data, topLevel = true) {
+  // break out if top level is not array or object
+  if (topLevel && !Array.isArray(data) && typeof data !== "object")
+    return undefined;
+
+  if (Number.isInteger(data)) return 1;
+  if (Array.isArray(data)) {
+    return data.reduce((count, item) => count + totalIntegers(item, false), 0);
+  }
+  if (data !== null && typeof data === "object") {
+    return Object.values(data).reduce(
+      (count, value) => count + totalIntegers(value, false),
+      0,
+    );
+  }
+  return 0;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
